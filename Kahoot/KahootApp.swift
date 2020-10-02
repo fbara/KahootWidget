57b0122d43c9a73ab41c09385d75458bb7fc843b
fbara <fbara1@yahoo.com>
2020-10-02 13:07:24 -0500

    Initial Commit

---
+++ Kahoot/KahootApp.swift
@@ -0,0 +1,17 @@
+//
+//  KahootApp.swift
+//  Kahoot
+//
+//  Created by Frank Bara on 10/2/20.
+//
+
+import SwiftUI
+
+@main
+struct KahootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
